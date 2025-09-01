d5f022400230a2d0dd740c6b8d6ad404d9cbf26a
micaiasviola <leo.micaias@gmail.com>
2025-09-01 12:53:30 -0300

    Alteração no visual

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function scrollToElement(elementSelector, instance = 0){
+// Função original para scroll
+function scrollToElement(elementSelector, instance = 0) {
     const elements = document.querySelectorAll(elementSelector);
-
-    if(elements.length > instance){
-        elements[instance].scrollIntoView({behavior: 'smooth'});
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
     }
 }
 
@@ -12,12 +12,68 @@ const link3 = document.getElementById("link3");
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
-})
+});
 
 link2.addEventListener('click', () => {
-    scrollToElement('.header', 1);//pula para o segundo elemente com a classe header
-})
+    scrollToElement('.header', 1);
+});
 
 link3.addEventListener('click', () => {
     scrollToElement('.column', 2);
-})
+});
+
+// Configuração para o particles.js
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "onresize": {
+                "enable": true
+            }
+        }
+    },
+    "retina_detect": true
+});
